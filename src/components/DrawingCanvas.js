487731b2046b87881963fb7d1f9e61233cf9a1ce
danyip <hello@danyip.com>
2022-03-12 00:05:49 +1100

    fixed size canvas working

--- src/components/DrawingCanvas.js
+++ src/components/DrawingCanvas.js
@@ -8,35 +8,50 @@ function DrawingCanvas() {
 
   const resize = () => {
     const ctx = canvas.current.getContext("2d");
-    ctx.canvas.width = window.innerWidth;
-    ctx.canvas.height = window.innerHeight;
+    // ctx.canvas.width = window.innerWidth;
+    // ctx.canvas.height = window.innerHeight;
+    ctx.canvas.width = 800;
+    ctx.canvas.height = 600;
   };
 
   const startDraw = (e) => {
     console.log("mouse down: start drawing", e);
     setDrawing(true);
+    
+  };
+
+  // TODO: figure out how to make this work to draw dots
+  // useEffect((e) => {
+  //   draw(e)
+  // }, [drawing])
+  
+
+  const stopDraw = () => {
+    console.log("mouse up: stop drawing");
+    setDrawing(false);
     const ctx = canvas.current.getContext("2d");
     ctx.beginPath();
   };
 
-  const stopDraw = (e) => {
-    console.log("mouse up: stop drawing");
-    setDrawing(false);
-  };
-
   const draw = (e) => {
+    // console.log('draw', e);
     if (!drawing) return;
+
+    const xPos = e.clientX - canvas.current.offsetLeft; //TODO: may need to account for scroll position here
+    const yPos = e.clientY -canvas.current.offsetTop; //TODO: may need to account for scroll position here
 
     const ctx = canvas.current.getContext("2d");
     ctx.lineWidth = 10;
     ctx.lineCap = "round";
     ctx.strokeStyle = "black";
 
-    ctx.lineTo(e.clientX, e.clientY);
+    // console.log(ctx);
+
+    ctx.lineTo(xPos, yPos);
     ctx.stroke();
     ctx.beginPath();
-    ctx.moveTo(e.clientX, e.clientY);
-
+    ctx.moveTo(xPos, yPos);
+    // console.log(xPos, yPos, canvas);
   };
 
   useEffect(() => {
@@ -45,7 +60,7 @@ function DrawingCanvas() {
     return () => window.removeEventListener("resize", resize);
   }, []);
 
-  
+
   return (
     <canvas
       className="canvas"
