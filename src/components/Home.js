dacc0913ba5b995d40dc1ebe2db58958526eea4e
danyip <hello@danyip.com>
2022-03-16 08:56:46 +1100

    added logout link

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,28 @@
 import React from 'react'
 import { Outlet, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 function Home() {
   const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const logout = ()=>{
+    dispatch({type: 'currentUser/logout'})
+    navigate('/login')
+  }
+
   return (
     <div>
     <header>
       <h1>imperfectionary</h1>
       <nav>
-        <div onClick={() => navigate('/draw')}>Draw</div>
-        <div onClick={() => navigate('/guess')}>Guess</div>
+        <ul>
+          <li onClick={() => navigate('/login')}>Login</li>
+          <li onClick={() => navigate('/signup')}>Signup</li>
+          <li onClick={() => navigate('/draw')}>Draw</li>
+          <li onClick={() => navigate('/guess')}>Guess</li>
+          <li onClick={logout}>Logout</li>
+        </ul>
       </nav>
     </header>
       <Outlet/>
