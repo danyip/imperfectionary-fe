1f9e4c878725917cde0ce916a63723b580b52b50
danyip <hello@danyip.com>
2022-03-15 20:05:40 +1100

    refactored email validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { emailRegex} from "../utils/regex";
 import "../stylesheets/forms.css";
 
 function Login() {
@@ -6,15 +7,8 @@ function Login() {
   const [emailFormatValidation, setEmailFormatValidation] = useState(true);
   const [password, setPassword] = useState("");
 
-  const checkEmailFormat = (e) => {
-    const emailRegex =
-      /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-
-    if (emailRegex.test(e.target.value)) {
-      setEmailFormatValidation(true);
-      return;
-    }
-    setEmailFormatValidation(false);
+  const checkEmailFormat = () => {
+    setEmailFormatValidation(emailRegex.test(email));
   };
 
   const handleLogin = (e) => {
